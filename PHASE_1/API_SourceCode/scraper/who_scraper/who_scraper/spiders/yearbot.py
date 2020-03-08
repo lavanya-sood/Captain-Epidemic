37ee1e35fc0412cb581d4b89e9d9c65c0100f6c4
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-03-08 18:58:24 +1100

    print year urls to json file

--- PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/spiders/yearbot.py
+++ PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/spiders/yearbot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class YearbotSpider(scrapy.Spider):
@@ -8,9 +9,13 @@ class YearbotSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.css('.col_2-1_1 a::attr(href)').extract()
+        jsonOutput = {'url': []}
         for u in urls:
             url = 'who.int/' + u
             scraped_info = {
                 'url': url
             }
             yield scraped_info
+            jsonOutput['url'].append(url)
+        with open('year-urls.txt', 'w') as yearUrls:
+            json.dump(jsonOutput, yearUrls)
