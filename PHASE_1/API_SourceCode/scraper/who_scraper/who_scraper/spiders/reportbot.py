a858e77bef06155c7277032776bcdaab730fd070
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-03-12 18:05:42 +1100

    fixed publication date

--- PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/spiders/reportbot.py
+++ PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/spiders/reportbot.py
@@ -10,17 +10,16 @@ class ReportbotSpider(scrapy.Spider):
     def parse(self, response):
         headline = response.css(".headline::text").extract()[0]
         
-        publication_date = re.findall('\d{4}_\d{2}_\d{2}', response.url)
-        if len(publication_date) == 0:
-            list_publication_date = response.url.split('don/')[1].split('-')[:3]
-            publication_date = '_'.join(list_publication_date)
-        
+        publication_date = response.xpath('//meta[@name="DC.date.published"]/@content')[0].extract()
         #convert yyyy_mm_dd and dd_month_yyyy for database
         
         maintext = response.css('div#primary').extract()[0].split('<h3 class="section_head1"')[0].split('<!-- close of the meta div -->')
         if len(maintext) == 1: 
             maintext = maintext[0]
-            maintext = re.sub('^ ', '', re.sub(' +', ' ', re.sub(r'<[^>]*?>', '', '\n'.join(''.join(maintext.replace('\n', ' ').replace('<span>','\n').split('</span>')[1:]).replace('\t','').split('\n')[1:]))))
+            if (len(response.css('.dateline').extract()) > 0):
+                maintext = re.sub('^ ', '', re.sub(' +', ' ', re.sub(r'<[^>]*?>', '', '\n'.join(''.join(maintext.replace('\n', ' ').replace('<span>','\n').split('</span>')[1:]).replace('\t','').split('\n')[0:]))))
+            else: 
+                maintext = re.sub('^ ', '', re.sub(' +', ' ', re.sub(r'<[^>]*?>', '', '\n'.join(''.join(maintext.replace('\n', ' ').replace('<span>','\n').split('</span>')[1:]).replace('\t','').split('\n')[1:]))))
         else:
             maintext = maintext[1]
             maintext = re.sub(r'<[^>]*?>', '', "\n".join(maintext.split('<span>')[1:])).replace('\n\t\t\n  \t\t\n  \t\t\n', '\n').rstrip()
@@ -41,21 +40,53 @@ class ReportbotSpider(scrapy.Spider):
         else:
             disease = [disease]
 
-        # event date is found within the main text, sometimes the date is not found
-        text = maintext
-        event_date_list = []
-        date_found = re.search(r'([0-9]{1,2} to )?[0-9]{1,2} (January|February|March|April|May|June|July|August|September|October|November|December)( [0-9]{4})?', text, re.IGNORECASE)
-        if (date_found):
-            date_found = date_found.group()
-            event_date_list.append(date_found)
-            while(date_found is not None):
-                text = text.replace(date_found, '')
-                date_found = re.search(r'([0-9]{1,2} to )?[0-9]{1,2} (January|February|March|April|May|June|July|August|September|October|November|December)( [0-9]{4})?', text, re.IGNORECASE)
-                if (date_found):
-                    date_found = date_found.group()
-                    event_date_list.append(date_found)
-                else:
-                    date_found = None
+        # event date is mostly found within the first paragraph of the main text, sometimes the date is not found so the second paragraph is scanned as well just in case
+        text = maintext.split('\n')[0]
+        event_date_list = event_date_helper(text)
+        if (len(event_date_list) == 0):
+            text = maintext.split('\n')[1]
+            event_date_list = event_date_helper(text)
+        # if there's more than one disease, find the date that matches the right disease
+            # same date
+            # different dates
+            # multiple dates
+        
+        
+        #need year of publication date
+        #publication_year = re.search('[0-9]{4}', publication_date).group()
+        month = {
+            'January': '1',
+            'February': '2',
+            'March': '3',
+            'April': '4',
+            'May': '5',
+            'June': '6',
+            'July': '7',
+            'August': '8',
+            'September': '9',
+            'October': '10',
+            'November': '11',
+            'December': '12'
+        }
+        months = '|'.join(month.keys())
+        # change dates to use ints and add /
+        #temp = []
+        #for event in event_date_list:
+        #    get_month = re.search(months, event).group()
+        #    event = event.replace(get_month, month[get_month])
+        #    event = event.replace(' ','/')
+        #    temp.append(event)
+        #event_date_list = temp
+
+        # change if there's 'and', 'or' between the dates
+            # remove any dates between these dates
+            # change to two dates with a - in between
+            # if there's more dates than theres 2 disease reports
+
+        # if a day and month matches another one then remove it 
+        # if a day and month doesn't have a year, add the publication year
+        # remove duplicate dates
+
 
         scraped_info = {
             'url': response.url,
@@ -66,3 +97,21 @@ class ReportbotSpider(scrapy.Spider):
             'eventdate': event_date_list
         }
         yield scraped_info
+
+        
+    
+def event_date_helper(text):
+    event_date_list = []
+    date_found = re.search(r'([0-9]{1,2} (to|and) )?([0-9]{1,2} )?(January|February|March|April|May|June|July|August|September|October|November|December)( (and|to) (January|February|March|April|May|June|July|August|September|October|November|December))?( [0-9]{4})?', text)
+    if (date_found):
+        date_found = date_found.group()
+        event_date_list.append(date_found)
+        while(date_found is not None):
+            text = text.replace(date_found, '')
+            date_found = re.search(r'([0-9]{1,2} (to|and) )?([0-9]{1,2} )?(January|February|March|April|May|June|July|August|September|October|November|December)( (and|to) (January|February|March|April|May|June|July|August|September|October|November|December))?( [0-9]{4})?', text)
+            if (date_found):
+                date_found = date_found.group()
+                event_date_list.append(date_found)
+            else:
+                date_found = None
+    return event_date_list
