dbc7c405e2def6f9be466d3d80faa22e0f2979b9
Lavanya Sood <lavanyasood09@gmail.com>
2020-03-12 16:28:14 +1100

    Fixed API Documentation

--- PHASE_1/API_SourceCode/api/api.py
+++ PHASE_1/API_SourceCode/api/api.py
@@ -1,7 +1,8 @@
 import flask
-from flask import request, jsonify
+from flask import request, jsonify,send_from_directory, make_response
 import sqlite3
 from flask_swagger_ui import get_swaggerui_blueprint
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -53,30 +54,48 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static',path)
 
-SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
 
-# Call factory function to create our blueprint
-swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+
+# SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+# #API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+# API_URL = '/static/swagger.json'
+# # Call factory function to create our blueprint
+# swaggerui_blueprint = get_swaggerui_blueprint(
+#     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+#     API_URL,
+#     config={  # Swagger UI config overrides
+#         'app_name': "Test application"
+#     },
+#     # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+#     #    'clientId': "your-client-id",
+#     #    'clientSecret': "your-client-secret-if-required",
+#     #    'realm': "your-realms",
+#     #    'appName': "your-app-name",
+#     #    'scopeSeparator': " ",
+#     #    'additionalQueryStringParams': {'test': "hello"}
+#     # }
+# )
+
+# # Register blueprint at URL
+# # (URL must match the one given to factory function above)
+# app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
     API_URL,
-    config={  # Swagger UI config overrides
-        'app_name': "Test application"
-    },
-    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
-    #    'clientId': "your-client-id",
-    #    'clientSecret': "your-client-secret-if-required",
-    #    'realm': "your-realms",
-    #    'appName': "your-app-name",
-    #    'scopeSeparator': " ",
-    #    'additionalQueryStringParams': {'test': "hello"}
-    # }
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
 )
-
-# Register blueprint at URL
-# (URL must match the one given to factory function above)
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 
 app.run()
