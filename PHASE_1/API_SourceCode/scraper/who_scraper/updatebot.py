8dd76268d8dc1a7965af52d5dd5f7a817dd1a7fe
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-03-19 13:20:11 +1100

    added update bot

--- PHASE_1/API_SourceCode/scraper/who_scraper/updatebot.py
+++ PHASE_1/API_SourceCode/scraper/who_scraper/updatebot.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from scrapy.crawler import CrawlerProcess
 from who_scraper.spiders.reportbot import ReportbotSpider
+import sqlite3
+import os
+import re
 
 c = urlopen('https://www.who.int/csr/don/archive/year/en/')
 contents = c.read()
@@ -11,12 +14,32 @@ date_links = soup.find_all('ul',{'class': 'list'})
 date = date_links[0].find('li')
 a = date.find('a')
 most_recent_year = 'https://www.who.int' + a['href']
+
+current_dir = os.getcwd()
+index = current_dir.index('scraper')
+database_path = current_dir[:index] + 'api\\who.db'
+
 c = urlopen(most_recent_year)
 contents = c.read()
 soup = BeautifulSoup(contents,'html.parser')
 actual_links = soup.find_all('ul',{'class':'auto_archive'})
+check = 0
+unscraped_links = []
 for a in actual_links[0].find_all('li'):
     for l in a.find_all('a'):
-        # check if database has url in the article report
-        # if yes then break
-        # if no then call report bot with the start_urls = [l]
+        link = 'https://www.who.int'+ l['href']
+        with sqlite3.connect(database_path) as db:
+            cursor = db.cursor()
+            sql = '''select URL from Article WHERE URL=?'''
+            cursor.execute(sql, (link,))
+            if (len(cursor.fetchall()) == 0):
+                unscraped_links.append(link)
+            else: 
+                check = 1
+                break
+    if (check == 1):
+        break
+
+process = CrawlerProcess()
+process.crawl(ReportbotSpider, start_urls=unscraped_links)
+process.start()
