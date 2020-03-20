c86ff8e3c0e0a2f7bd529783f3119062c2ca3ddf
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-03-20 22:33:22 +1100

    added testing urls back

--- PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/spiders/reportbot.py
+++ PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/spiders/reportbot.py
@@ -10,7 +10,7 @@ import string
 
 class ReportbotSpider(scrapy.Spider):
     name = 'reportbot'
-    start_urls = ['https://www.who.int/csr/don/2006_03_17/en/']
+    start_urls = ['https://www.who.int/csr/don/6-november-2017-dengue-burkina-faso/en/','https://www.who.int/csr/don/2010_10_25a/en/','https://www.who.int/csr/don/2014_01_09_h5n1/en/','https://www.who.int/csr/don/2014_07_17_polio/en/','https://www.who.int/csr/don/2014_08_06_ebola/en/','https://www.who.int/csr/don/2014_07_17_ebola/en/','https://www.who.int/csr/don/05-March-2020-ebola-drc/en/','https://www.who.int/csr/don/1996_11_28c/en/','https://www.who.int/csr/don/2014_6_23polio/en/','https://www.who.int/csr/don/2014_01_09_h5n1/en/','https://www.who.int/csr/don/04-march-2020-measles-car/en/', 'https://www.who.int/csr/don/2008_12_26a/en/', 'https://www.who.int/csr/don/2013_11_26polio/en/', 'https://www.who.int/csr/don/28-september-2015-cholera/en/', 'https://www.who.int/csr/don/05-october-2018-monkeypox-nigeria/en/', 'https://www.who.int/csr/don/2010_04_30a/en/', 'https://www.who.int/csr/don/2008_01_02/en/', 'https://www.who.int/csr/don/2006_08_21/en/', 'https://www.who.int/csr/don/2003_09_30/en/', 'https://www.who.int/csr/don/2001_07_18/en/', 'https://www.who.int/csr/don/1999_12_22/en/', 'https://www.who.int/csr/don/1996_02_29b/en/', 'https://www.who.int/csr/don/19-december-2016-1-mers-saudi-arabia/en/', 'https://www.who.int/csr/don/06-october-2016-polio-nigeria/en/', 'https://www.who.int/csr/don/12-january-2020-novel-coronavirus-china/en/','https://www.who.int/csr/don/03-june-2016-oropouche-peru/en/']
     
     def parse(self, response):
         headline = response.css(".headline::text").extract()[0]
