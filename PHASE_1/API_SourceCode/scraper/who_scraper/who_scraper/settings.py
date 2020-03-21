3936d7600c5379a4dd59b8033dc3fe74aa1eb0af
amandali031199 <41562483+amandali031199@users.noreply.github.com>
2020-03-21 17:51:30 +1100

    added pipelines

--- PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/settings.py
+++ PHASE_1/API_SourceCode/scraper/who_scraper/who_scraper/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'who_scraper.pipelines.WhoScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'who_scraper.pipelines.WhoScraperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,4 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
