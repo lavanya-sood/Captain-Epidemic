d26d11560325458ec79fadf71093335c25de0468
Nicole <z5211373@unsw.edu.au>
2020-03-21 14:13:44 +1100

    done post method

--- PHASE_1/API_SourceCode/api/api.py
+++ PHASE_1/API_SourceCode/api/api.py
@@ -5,6 +5,7 @@ import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_restplus import Api, Resource, fields,marshal
+from flask_restful import reqparse
 import datetime
 import re
 import json
@@ -18,6 +19,7 @@ api = Api(app,title=app.config.SWAGGER_UI_OAUTH_APP_NAME,description="This API c
 
 #api = Api(app,default='article',default_label='WHO Disease Article Operations',title=app.config.SWAGGER_UI_OAUTH_APP_NAME,description="This API can be used to access news articles from the WHO website. The WHO news articles have been scraped and separated into disease reports in the hopes of detecting epidemics by collecting global disease data. Disease reports can be accessed using GET requests whilst the POST, PUT and DELETE request can be accessed by authorised users which manipulates the scraped data stored within an SQL database.")
 
+parser = reqparse.RequestParser()
 
 api = api.namespace('article', description = 'WHO Disease Article and Report Operations')
 
@@ -49,12 +51,26 @@ parser1.add_argument('end_date', help='End date for the articles. Use format YYY
 parser1.add_argument('timezone', type=str, default='AEDT',
                                 choices=('ADT', 'AEDT', 'AEST', 'AET', 'MEST', 'UTC', 'WAST', 'WAT', 'WEST', 'WGT', 'WST'),
                                 help='Timezone to filter Who news articles by')
+#DELETE
 parser2 = api.parser()
 parser2.add_argument('id', help='Authorisation id to delete an existing article (only available to authorised users)', location='args', required=True)
 parser2.add_argument('url', help='Url to the Who news article to be deleted. Url must exist in the database', location='args', required=True)
+
+#POST
 parser3 = api.parser()
 parser3.add_argument('id', help='Authorisation id to post an article (only available to authorised users)', location='args', required=True)
 parser3.add_argument('url', help='Url to a Who news article. Must not already exist in the database', location='args', required=True)
+parser3.add_argument('date_of_publication', help="Date the Who news article was published. Use format YYYY-MM-DD hh:mm:ss e.g. '2020-01-17 13:09:44'", location='args')
+parser3.add_argument('headline', help="Headline of the Who news article", location='args')
+parser3.add_argument('main_text', help="Main text body of the Who news article", location='args')
+parser3.add_argument('event_date', help="Event date of the Who news report. Use format YYYY-MM-DD hh:mm:ss", location='args')
+parser3.add_argument('location', help="The location where the epidemic takes place. Eg: Milan", location='args')
+parser3.add_argument('country', help="The country where the epidemic takes place. Eg: Italy", location='args')
+parser3.add_argument('disease', help="The name of the disease. Eg: Ebola", location='args')
+parser3.add_argument('syndrome', help="The syndrome of the disease. Eg: Fever", location='args')
+
+
+#PUT
 parser4 = api.parser()
 parser4.add_argument('id', help='Authorisation id to put a disease report into an existing article (only available to authorised users)', location='args', required=True)
 parser4.add_argument('url', help='Url to the Who news article a report is to be added to. Url must exist in the database', location='args', required=True)
@@ -62,7 +78,7 @@ class Article(Resource):
     @api.response(200, 'Success',[articles])
     @api.response(404, 'No data found')
     @api.doc(params={'key_terms': 'The key terms to look for when finding article. Separate multiple key terms by comma. Eg:ebola,virus'})
-    @api.doc(params={'location': 'The country where the epidemic takes place. Eg: Guinea'})
+    @api.doc(params={'location': 'The location or country where the epidemic takes place. Eg: Guinea'})
     @api.response(400, 'Invalid date format')
     @api.doc(summary='Get request gets all the articles given the parameters')
     @api.expect(parser1,validate=False)
@@ -72,9 +88,15 @@ class Article(Resource):
         end_date = args['end_date']
         # check start and end date format
         if not re.match(r"^[0-9]{4}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$", start_date):
-            return "Invalid date input",400
+            return {
+                'message' : 'Invalid date input',
+                'status' : 400
+            }
         if not re.match(r"^[0-9]{4}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$", end_date):
-            return "Invalid date input",400
+            return {
+                'message' : 'Invalid date input',
+                'status' : 400
+            }
         location = request.args.get('location')
         if not location:
             location = ""
@@ -83,12 +105,104 @@ class Article(Resource):
             key_terms = ""
         final_start,final_end = self.convert_date_to_int(start_date,end_date)
         if final_end < final_start:
-            return "End date must be larger than start date",400
+            return {
+                'message' : 'End date must be larger than start date',
+                'status' : 400
+            }
         articles = self.check_data_exists(final_start,final_end,location,key_terms)
+        #return articles
         if articles == False:
-            return "No data found",404
+            return {
+                'message' : 'No data found',
+                'status' : 404
+            }
         result = self.get_results(articles)
-        return result,200
+        return {
+            'result' : result,
+            'status' : 200
+        }
+
+
+    @api.response(400, 'Invalid data given')
+    @api.response(403, 'url already exists')
+    @api.response(401, 'Unauthorised id')
+    @api.response(200, 'Success')
+    @api.expect(parser3,validate=True)
+    def post(self):
+        parser.add_argument('url', type=str)
+        parser.add_argument('id', type=str)
+        parser.add_argument('date_of_publication', type=str)
+        parser.add_argument('headline', type=str)
+        parser.add_argument('main_text', type=str)
+        parser.add_argument('event_date', type=str)
+        parser.add_argument('location', type=str)
+        parser.add_argument('country', type=str)
+        parser.add_argument('disease', type=str)
+        parser.add_argument('syndrome', type=str)
+
+        args = parser.parse_args()
+        # return 401 if authorization code is wrong
+        if args['id'] != '1810051939':
+            return {
+                'message' : 'Invalid authentication id',
+                'status' : 401
+            }
+        # return 400 if url is empty
+        if not args['url'] :
+            return {
+                'message' : 'Invalid input key in body',
+                'status' : 400
+            }
+        # check if url exist already
+        conn = sqlite3.connect('who.db')
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+        # only date given
+        query = 'SELECT * from Article where url = \'' + args['url'] + '\';'
+        results = cur.execute(query).fetchall()
+        if len(results) > 0:
+            return {
+                'message' : 'Url already exists',
+                'status' : 403
+            }
+        cur.close()
+        # insert data into db
+        conn = sqlite3.connect('who.db')
+        with conn:
+            # insert article
+            sql = ''' INSERT INTO Article(url,headline,date_of_publication,main_text) VALUES(?,?,?,?) '''
+            val = (args['url'], args['date_of_publication'],args['date_of_publication'],args['main_text']);
+            cur2 = conn.cursor()
+            cur2.execute(sql, val)
+            # insert report
+            sql = ''' INSERT INTO Report (url,event_date) VALUES(?,?) '''
+            val = (args['url'], args['event_date']);
+            cur = conn.cursor()
+            cur.execute(sql, val)
+            # insert disease
+            if args['disease']:
+                sql = ''' INSERT INTO Disease(Disease,ReportID) VALUES(?,?) '''
+                val = (args['disease'], cur.lastrowid);
+                cur3 = conn.cursor()
+                cur3.execute(sql, val)
+            # insert Country/location
+            if args['country'] or args['location']:
+                sql = ''' INSERT INTO Location(Location,Country,ReportID) VALUES(?,?,?) '''
+                val = (args['location'],args['country'],cur.lastrowid);
+                cur4 = conn.cursor()
+                cur4.execute(sql, val)
+            # insert syndrome
+            if args['syndrome']:
+                sql = ''' INSERT INTO Syndrome(Symptom,ReportID) VALUES(?,?) '''
+                val = (args['syndrome'], cur.lastrowid);
+                cur5 = conn.cursor()
+                cur5.execute(sql, val)
+
+        return {
+            'message': 'Article successfully added ',#as id = ' + str(cur.lastrowid),
+            'code' : 200
+        }
+
 
     @api.response(403, 'url does not exist')
     @api.response(401, 'Unauthorised id')
@@ -97,16 +211,6 @@ class Article(Resource):
     def delete(self, id):
          api.abort(401)
 
-    @api.doc(params={'date_of_publication': "Date the Who news article was published. Use format YYYY-MM-DD hh:mm:ss e.g. '2020-01-17 13:09:44'"})
-    @api.doc(params={'headline': 'Headline of the Who news article'})
-    @api.doc(params={'main-text': 'Main text body of the Who news article'})
-    @api.response(400, 'Invalid date_of_publication format')
-    @api.response(403, 'url already exists')
-    @api.response(401, 'Unauthorised id')
-    @api.response(200, 'Success')
-    @api.expect(parser3,validate=False)
-    def post(self):
-        api.abort(401)
 
     # adds a report to an article
     @api.doc(params={'event_date': "The date or date range the diseases were reported. Use format YYYY-MM-DD e.g. '2020-01-03' or '2018-12-01 to 2018-12-10'"})
@@ -127,32 +231,48 @@ class Article(Resource):
         conn = sqlite3.connect('who.db')
         conn.row_factory = dict_factory
         cur = conn.cursor()
+        # only date given
         query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ';'
+        # key terms and location given
         if location != '' and key_terms != '':
             if ',' in key_terms:
                 k = key_terms.split(',')
                 i = 1
-                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Disease d on d.ReportID = r.id JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and d.Disease = \'' + k[0].lower() + '\'and l.location = \'' + location.title() + '\''
+                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id JOIN SearchTerm s on s.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.location = \'' + location.title() + '\'  and s.SearchTerm = \'' + k[0].lower() + '\' '
+                query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN SearchTerm s on s.ReportID = r.id JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and s.SearchTerm = \'' + k[0].lower() + '\'and l.country = \'' + location.title() + '\' '
                 while i < len(k):
-                    query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Disease d on d.ReportID = r.id JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and d.Disease = \'' + k[i].lower() + '\'and l.location = \'' + location.title() + '\''
+                    query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id JOIN SearchTerm s on s.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.location = \'' + location.title() + '\'  and s.SearchTerm = \'' + k[i].lower() + '\' '
+                    query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN SearchTerm s on s.ReportID = r.id JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and s.SearchTerm = \'' + k[i].lower() + '\'and l.country = \'' + location.title() + '\' '
                     i+=1
                 query = query + ';'
             else:
-                query = 'SELECT * from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id JOIN Disease d on d.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.location = \'' + location.title() + '\'  and d.Disease = \'' + key_terms.lower() + '\';'
+                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id JOIN Disease d on d.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.location = \'' + location.title() + '\'  and d.Disease = \'' + key_terms.lower() + '\' '
+                query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Disease d on d.ReportID = r.id JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and s.SearchTerm = \'' + key_terms.lower() + '\'and l.country = \'' + location.title() + '\';'
+        #location only
         elif location != '':
-            query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,l.location,r.event_date from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.location = \'' + location.title() + '\';'
+            query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.location = \'' + location.title() + '\' '
+            query = query + 'UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN Location l on l.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and l.country = \''   + location.title() + '\';'
+        # key terms only
         elif key_terms != '':
             if ',' in key_terms:
                 k = key_terms.split(',')
                 i = 1
-                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Disease d on d.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and d.Disease = \'' + k[0].lower() + '\''
+                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN SearchTerm s on s.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and s.SearchTerm = \'' + k[0].lower() + '\' '
                 while i < len(k):
-                    query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Disease d on d.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and d.Disease = \'' + k[i].lower() + '\''
+                    query = query + ' UNION SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN SearchTerm s on s.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and s.SearchTerm = \'' + k[i].lower() + '\' '
                     i+=1
                 query = query + ';'
             else:
-                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date,d.disease from Article a JOIN Report r on r.url = a.url JOIN Disease d on d.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and d.Disease = \'' + key_terms.lower() + '\';'
+                query = 'SELECT r.id,a.headline,a.main_text,a.date_of_publication,a.url,r.event_date from Article a JOIN Report r on r.url = a.url JOIN SearchTerm s on s.ReportID = r.id where a.date_of_publication >=' + start_date + ' and a.date_of_publication <=' + end_date + ' and s.SearchTerm = \'' + key_terms.lower() + '\';'
         results = cur.execute(query).fetchall()
+        # filter if duplicate reports
+        id_list = []
+        filtered_location = []
+        for r in results:
+            if r['id'] not in id_list:
+                id_list.append(r['id'])
+                filtered_location.append(r)
+        results = filtered_location
         articles = {}
         if len(results) == 0:
             return False
