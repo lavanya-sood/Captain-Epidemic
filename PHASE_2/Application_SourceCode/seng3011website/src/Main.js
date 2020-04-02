ab255e81dabcc823d5e839bd7cc208fc149c1e8f
Lavanya Sood <lavanyasood09@gmail.com>
2020-04-03 02:00:07 +1100

    Added page footer

--- PHASE_2/Application_SourceCode/seng3011website/src/Main.js
+++ PHASE_2/Application_SourceCode/seng3011website/src/Main.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Game from "./Game";
+import Profile from "./Profile";
 import heading from './img/webheading.png';
 import logo from './img/Logo.png';
 import userimg from './img/user.png';
@@ -9,19 +17,22 @@ import {Navbar,Nav,Form,FormControl,Button,Dropdown} from 'react-bootstrap';
 class Main extends Component {
   render() {
     return (
+      <HashRouter>
         <div>
-          <a href='/home'><img src={heading} id='heading' alt=""/></a>
-          <Navbar id='navbar' expand="lg" href='/home'>
-            <a href='/home' className="d-inline-block align-top" ><img src={logo} id="logo" href='/home'alt="Website logo"/> </a>
+          <a href='/'><img src={heading} id='heading'/></a>
+          
+          <Navbar id='navbar' expand="lg" href='/'>
+            <a href='/' className="d-inline-block align-top" ><img src={logo} id="logo" href='/'alt="Website logo"/> </a>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Link class='searchbar'>
                   <Form inline>
                     <FormControl type="text" placeholder="Search" className="searchbox" />
-                    <Button className='navbutton'> < img className="nav-image" src={search} alt=""/></Button>
+                    <Button className='navbutton'> < img className="nav-image" src={search} /></Button>
                   </Form>
                 </Nav.Link>
+              
               </Nav>
               <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-custom">
@@ -32,9 +43,7 @@ class Main extends Component {
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                <Dropdown.Item href="/game">Game</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+                  <Dropdown.Item href="/">Profile</Dropdown.Item>
                   <Dropdown.Divider />
                   <Dropdown.Item href="/">Logout</Dropdown.Item>
                 </Dropdown.Menu>
@@ -51,10 +60,16 @@ class Main extends Component {
             <form> <input type="text" name="name" /> <input type="submit" value="Submit"/> </form>
             </li>
             <li><NavLink to="/profile">Profile</NavLink></li>
-          </ul> 
-          <div className="content">
-          </div>*/}
+          </ul> */}
+          {/* <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/Game" component={Game}/>
+            <Route path="/Profile" component={Profile}/>
+          </div> */}
+
+          
         </div>
+      </HashRouter>
     );
   }
 }
