5b1d1df59e2e4ed4785e6928b1b2864896b1e659
Nicole <z5211373@unsw.edu.au>
2020-04-03 22:41:31 +1100

    update font at quiz page

--- PHASE_2/Application_SourceCode/seng3011website/src/Games/Quiz.js
+++ PHASE_2/Application_SourceCode/seng3011website/src/Games/Quiz.js
@@ -2,7 +2,7 @@
 import React, { Component, Fragment } from "react";
 import mainLayout from '../MainLayout.js';
 import quizdata from './question'
-import Answer from "../Answer";
+import Answer from "./Answer";
 import logo from '../img/virus.png';
 
 class Quiz extends Component{
@@ -32,13 +32,15 @@ class Quiz extends Component{
       <Fragment>
       <div className="quiz-div">
       <img src={logo} className = "quiz-img" alt="Logo" width="110" height="100" />
-      <h3 className = "quiz-name">Quiz</h3>
+      <div className = "quiz-name">Quiz</div>
       </div>
          {
           this.state.dataQuestion.map( data => {
           return <div key={ data.id} >
-                      <div className = "question"> <h4> { data.question}</h4> </div>
-                     <Answer key={ data.id} id = {data.id} correct={ data.correct} ans={ data.answers} />
+                      <div className = "question">  { data.question} </div>
+                      <div className = "answers">
+                     <Answer  key={ data.id} id = {data.id} correct={ data.correct} ans={ data.answers} />
+                    </div>
                  </div>
           })
          }
