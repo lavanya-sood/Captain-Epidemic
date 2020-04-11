67b10ee6fcb075404232b6f5913925e4e7b57b57
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-04-11 22:34:39 +1000

    added teletubbies api call

--- PHASE_2/Application_SourceCode/api/app.js
+++ PHASE_2/Application_SourceCode/api/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var mapRouter = require('./routes/map');
+var testRouter = require('./routes/testing');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/map', mapRouter);
+app.use('/testing', testRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- PHASE_2/Application_SourceCode/seng3011website/src/Games/Game.js
+++ PHASE_2/Application_SourceCode/seng3011website/src/Games/Game.js
@@ -3,11 +3,22 @@ import { Link} from "react-router-dom";
 import logo from '../img/virus.png';
 import mainLayout from '../MainLayout.js';
 class Game extends Component {
+  state = { users : "" }
+    
+   callAPI() {
+        fetch("http://localhost:9000/testing")
+            .then(res => res.json())
+            .then(res => this.setState({ users: res}));
+    }
+    componentWillMount() {
+        this.callAPI();
+    }
 
   render() {
     return (
       <div>
         <h2>The Virus Game</h2>
+        <p>{this.state.users}</p>
         <div>
         <img src={logo} alt="Logo" width="110" height="100" />
         </div>
