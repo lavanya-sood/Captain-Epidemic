aeed554331c8e452d35841c0fe2936bef5261a7f
sarahoakman <41679973+sarahoakman@users.noreply.github.com>
2020-04-23 23:53:01 +1000

    added error checking for disease url

--- PHASE_2/Application_SourceCode/seng3011website/src/map/Leaflet.js
+++ PHASE_2/Application_SourceCode/seng3011website/src/map/Leaflet.js
@@ -118,15 +118,9 @@ class LeafletMap extends Component {
             .then(res => res.json())
             .then(res => this.setState({ countries: res }))
     }
-    /*callDiseaseAPI() {
-        fetch("/map/diseases")
-            .then(res => res.json())
-            .then(res => this.setState({ diseases: res }))
-    }*/
     componentDidMount() {
         this.callAPI();
         this.callLocationAPI();
-        //this.callDiseaseAPI();
         if (this.props.data) {
             this.setState({
                 lat: this.props.data.lat,
