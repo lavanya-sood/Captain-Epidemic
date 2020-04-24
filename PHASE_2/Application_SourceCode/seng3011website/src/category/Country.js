537e877e7e41363c3b7c4b025b0aab5485c23fe2
yangyuner6 <yangyuner6@gmail.com>
2020-04-24 13:49:58 +1000

    hardcode location category page

--- PHASE_2/Application_SourceCode/seng3011website/src/category/Country.js
+++ PHASE_2/Application_SourceCode/seng3011website/src/category/Country.js
@@ -5,6 +5,8 @@ import logo from "../img/virus5.png";
 import aus from "../img/aus.png";
 import china from "../img/china1.png";
 import usa from "../img/usa.png";
+import germany from "../img/germany.png";
+import italy from "../img/italy.png";
 import "../css/pure-min.css";
 import "../css/SearchResult.css";
 
@@ -13,24 +15,19 @@ class Country extends Component {
     countries: "",
   };
   callLocationAPI() {
-    fetch("/map/countries")
+    fetch("/location/countries")
       .then((res) => res.json())
       .then((res) => this.setState({ countries: res }));
   }
-  listLocations = () => {
-    let container = [];
-    for (let i = 0; i < this.state.countries.length; i++) {
-      container.push(
-        <Link to="location/{this.state.countries[i]}" className="result-link">
-          <div className="result">
-            {/* <img src={aus} className="result-img"></img> */}
-            <h3 className="result-content">{this.state.countries[i]} </h3>
-          </div>
-        </Link>
-      );
-    }
-    return container;
-  };
+  // listLocations = () => {
+  //   let container = [];
+  //   for (let i = 0; i < this.state.countries.length; i++) {
+  //     container.push(
+
+  //     );
+  //   }
+  //   return container;
+  // };
   render() {
     return (
       <div className="result-div">
@@ -44,7 +41,37 @@ class Country extends Component {
           />
           <h2 className="header-name">Locations</h2>
         </div>
-        <div> {this.listLocations()} </div>
+        {/* <div> {this.listLocations()} </div> */}
+        <Link to="location/Australia" className="result-link">
+          <div className="result">
+            <img src={aus} className="result-img"></img>
+            <h3 className="result-content">Australia</h3>
+          </div>
+        </Link>
+        <Link to="location/China" className="result-link">
+          <div className="result">
+            <img src={china} className="result-img"></img>
+            <h3 className="result-content">China</h3>
+          </div>
+        </Link>
+        <Link to="location/America" className="result-link">
+          <div className="result">
+            <img src={usa} className="result-img"></img>
+            <h3 className="result-content">USA</h3>
+          </div>
+        </Link>
+        <Link to="location/Germany" className="result-link">
+          <div className="result">
+            <img src={germany} className="result-img"></img>
+            <h3 className="result-content">Germany</h3>
+          </div>
+        </Link>
+        <Link to="location/Italy" className="result-link">
+          <div className="result">
+            <img src={italy} className="result-img"></img>
+            <h3 className="result-content">Italy</h3>
+          </div>
+        </Link>
       </div>
     );
   }
