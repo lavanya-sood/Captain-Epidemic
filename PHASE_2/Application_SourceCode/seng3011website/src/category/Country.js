0512667d77d5de38c175617b3ce4e37d82fa6d6a
yangyuner6 <yangyuner6@gmail.com>
2020-04-24 16:30:25 +1000

    added country and disease category sorted

--- PHASE_2/Application_SourceCode/seng3011website/src/category/Country.js
+++ PHASE_2/Application_SourceCode/seng3011website/src/category/Country.js
@@ -12,24 +12,210 @@ import "../css/pure-min.css";
 import "../css/SearchResult.css";
 
 class Country extends Component {
-  state = {
-    countries: "",
-  };
-  callLocationAPI() {
-    fetch("/location/countries")
-      .then((res) => res.json())
-      .then((res) => this.setState({ countries: res }));
-  }
-  // listLocations = () => {
-  //   let container = [];
-  //   for (let i = 0; i < this.state.countries.length; i++) {
-  //     container.push(
+  locations = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bhutan",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "Comoros",
+    "Congo",
+    "Cook Islands",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Curaçao",
+    "Cyprus",
+    "Côte d'Ivoire",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Ethiopia",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Guiana",
+    "French Polynesia",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Greece",
+    "Guadeloupe",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran, Islamic Republic of",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Lao People's Democratic Republic",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Lithuania",
+    "Luxembourg",
+    "Macao",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Martinique",
+    "Mauritania",
+    "Mauritius",
+    "Mayotte",
+    "Mexico",
+    "Mongolia",
+    "Montenegro",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nepal",
+    "Netherlands",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Romania",
+    "Russian Federation",
+    "Rwanda",
+    "Réunion",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Taiwan, Province of China",
+    "Tajikistan",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Uruguay",
+    "Uzbekistan",
+    "Vietnam",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+  ];
 
-  //     );
-  //   }
-  //   return container;
-  // };
   render() {
+    let container = [];
+    for (let i = 0; i < this.locations.length; i++) {
+      const link = "info/" + this.locations[i];
+      container.push(
+        <Link to={link} className="result-link">
+          <div className="result">
+            {/* <img src={aus} className="result-img"></img> */}
+            <h3 className="result-content"> {this.locations[i]} </h3>
+          </div>
+        </Link>
+      );
+    }
+
     return (
       <div className="result-div">
         <div className="header-div">
@@ -42,37 +228,7 @@ class Country extends Component {
           />
           <h2 className="header-name">Locations</h2>
         </div>
-        {/* <div> {this.listLocations()} </div> */}
-        <Link to="location/Australia" className="result-link">
-          <div className="result">
-            <img src={aus} className="result-img"></img>
-            <h3 className="result-content">Australia</h3>
-          </div>
-        </Link>
-        <Link to="location/China" className="result-link">
-          <div className="result">
-            <img src={china} className="result-img"></img>
-            <h3 className="result-content">China</h3>
-          </div>
-        </Link>
-        <Link to="location/America" className="result-link">
-          <div className="result">
-            <img src={usa} className="result-img"></img>
-            <h3 className="result-content">USA</h3>
-          </div>
-        </Link>
-        <Link to="location/Germany" className="result-link">
-          <div className="result">
-            <img src={germany} className="result-img"></img>
-            <h3 className="result-content">Germany</h3>
-          </div>
-        </Link>
-        <Link to="location/Italy" className="result-link">
-          <div className="result">
-            <img src={italy} className="result-img"></img>
-            <h3 className="result-content">Italy</h3>
-          </div>
-        </Link>
+        <div> {container} </div>
       </div>
     );
   }
